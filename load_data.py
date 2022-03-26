df095472837f8745619f82b9f72a494461d2bc51
APerezFadon <49376581+APerezFadon@users.noreply.github.com>
2022-03-26 17:32:56 +0000

    Add files via upload

--- load_data.py
+++ load_data.py
@@ -27,4 +27,48 @@ def load(satellite):
 	# return df
 
 if __name__ == "__main__":
-	plot_data(load("envisat"))
+	load("envisat")
+
+"""
+        frames=[go.Frame(
+            data=[
+                go.Scatter3d(
+                    x=[data1[k, 0]],
+                    y=[data1[k, 1]],
+                    z=[data1[k, 2]],
+                    mode="markers",
+                    name="Envisat",
+                    marker=dict(color="red", size=2)),
+                go.Scatter3d(
+                    x=[data2[k, 0]],
+                    y=[data2[k, 1]],
+                    z=[data2[k, 2]],
+                    mode="markers",
+                    name="Globalstar",
+                    marker=dict(color="green", size=2)),
+                go.Scatter3d(
+                    x=[data3[0, 0]],
+                    y=[data3[0, 1]],
+                    z=[data3[0, 2]],
+                    mode="markers",
+                    name="Earth",
+                    marker=dict(color="blue", size=20)),
+                go.Scatter3d(
+                    x=[data1[max(k-200, 0):k+1, 0]],
+                    y=[data1[max(k-200, 0):k+1, 1]],
+                    z=[data1[max(k-200, 0):k+1, 2]],
+                    mode="lines",
+                    line=dict(color="red", width=5)),
+                go.Scatter3d(
+                    x=[data2[max(k-200, 0):k+1, 0]],
+                    y=[data2[max(k-200, 0):k+1, 1]],
+                    z=[data2[max(k-200, 0):k+1, 2]],
+                    mode="lines",
+                    line=dict(color="green", width=5)),
+            ],
+            layout={
+                "xaxis": {"range": [-axisScale * Re, axisScale * Re]},
+                "yaxis": {"range": [-axisScale * Re, axisScale * Re]}
+            }
+            )for k in range(len(data1))])
+"""
--- test_3d.py
+++ test_3d.py
@@ -5,15 +5,10 @@ import numpy as np
 import plotly.graph_objects as go
 from load_data import load
 
-# df = px.data.iris()
-
-# df_numpy = np.array([[0., 0., 0.], [1., 1., 1.]])
-# df = pd.DataFrame(df_numpy, columns = ['x','y',"z"])
-
-Re = 6.371e6
 
 def plot_data(data1, data2, data3):
-    axisD = 10E6
+    Re = 6371000
+    axisScale = 1.5
     fig = go.Figure(
         data=[
             go.Scatter3d(
@@ -23,7 +18,6 @@ def plot_data(data1, data2, data3):
                 mode='lines',
                 name="Envisat",
                 line=dict(
-                    # size=size,
                     color="orange",
                     colorscale='Viridis')),
             go.Scatter3d(
@@ -33,7 +27,6 @@ def plot_data(data1, data2, data3):
                 mode='lines',
                 name="Globalstar",
                 line=dict(
-                    # size=size,
                     color="green",
                     colorscale='Viridis')),
             go.Scatter3d(
@@ -45,41 +38,87 @@ def plot_data(data1, data2, data3):
                 marker=dict(
                     size=20,
                     color="blue",
-                    colorscale='Viridis'))],
+                    colorscale='Viridis')),
+            go.Scatter3d(
+                x=[data1[0, 0]],
+                y=[data1[0, 1]],
+                z=[data1[0, 2]],
+                mode="markers",
+                name="Envisat",
+                marker=dict(color="red", size=2)),
+            go.Scatter3d(
+                x=[data2[0, 0]],
+                y=[data2[0, 1]],
+                z=[data2[0, 2]],
+                mode="markers",
+                name="Globalstar",
+                marker=dict(color="green", size=2))],
+
+        layout=go.Layout(
+
+            title="Start Title",
+            hovermode="closest",
+            updatemenus=[dict(
+                type="buttons",
+                buttons=[dict(label="Play",
+                              method="animate",
+                              args=[None, {"frame": {"duration": 5}, 
+                                  "transition": {"duration": 5}}])
+                         ])]),
 
         frames=[go.Frame(
-            # layout=go.layout.scene(
-            #     xaxis=dict(range=[-8E4, 8E4], autorange=False, zeroline=False),
-            #     yaxis=dict(range=[-8E4, 8E4], autorange=False, zeroline=False),
-            #     zaxis=dict(range=[-8E4, 8E4], autorange=False, zeroline=False)
-            # ),
             data=[
+                go.Scatter3d(
+                    x=data1[max(k-100, 0):k+1, 0],
+                    y=data1[max(k-100, 0):k+1, 1],
+                    z=data1[max(k-100, 0):k+1, 2],
+                    mode="lines",
+                    line=dict(color="red", colorscale='Viridis')),
+                go.Scatter3d(
+                    x=data2[max(k-100, 0):k+1, 0],
+                    y=data2[max(k-100, 0):k+1, 1],
+                    z=data2[max(k-100, 0):k+1, 2],
+                    mode="lines",
+                    line=dict(color="green", colorscale='Viridis')),
+                go.Scatter3d(
+                    x=[data3[0, 0]],
+                    y=[data3[0, 1]],
+                    z=[data3[0, 2]],
+                    mode="markers",
+                    name="Earth",
+                    marker=dict(color="blue", size=20)),
                 go.Scatter3d(
                     x=[data1[k, 0]],
                     y=[data1[k, 1]],
                     z=[data1[k, 2]],
                     mode="markers",
-                    marker=dict(color="red", size=10)),
+                    name="Envisat",
+                    marker=dict(color="red", size=2)),
                 go.Scatter3d(
                     x=[data2[k, 0]],
                     y=[data2[k, 1]],
                     z=[data2[k, 2]],
                     mode="markers",
-                    marker=dict(color="red", size=10))
+                    name="Globalstar",
+                    marker=dict(color="green", size=2)),
+            ],
+            layout={
+                "xaxis": {"range": [-axisScale * Re, axisScale * Re]},
+                "yaxis": {"range": [-axisScale * Re, axisScale * Re]}
+            }
+            )for k in range(len(data1))])
 
-            ])for k in range(len(data1))])
-
-    ##########
     fig.update_layout(
+        paper_bgcolor="black",
         scene=dict(
-            xaxis=dict(nticks=4, range=[-5 * Re, 5 * Re], visible = False),
-            yaxis=dict(nticks=4, range=[-5 * Re, 5 * Re], visible = False),
-            zaxis=dict(nticks=4, range=[-5 * Re, 5 * Re], visible = False),),
-        )#width=700,
-        #margin=dict(r=20, l=10, b=10, t=10))
-        
-    fig.update_layout(paper_bgcolor="black")
-    ###########
+            xaxis=dict(nticks=4, range=[-axisScale *
+                       Re, axisScale * Re], autorange=False, visible = False),
+            yaxis=dict(nticks=4, range=[-axisScale *
+                       Re, axisScale * Re], autorange=False, visible = False),
+            zaxis=dict(nticks=4, range=[-axisScale * 
+            	       Re, axisScale * Re], autorange=False, visible = False),
+            aspectratio = dict(x=1, y=1, z=1))
+    )
 
     app = Dash(__name__)
 
@@ -98,5 +137,9 @@ def plot_data(data1, data2, data3):
 
 
 if __name__ == '__main__':
+
     plot_data(load("envisat"), load("globalstar"), np.array([[0., 0., 0.]]))
+
+
+
 
