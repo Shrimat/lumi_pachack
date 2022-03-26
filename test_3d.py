653cccebf5798aec5a0943035fc80be475f9e60a
APerezFadon <49376581+APerezFadon@users.noreply.github.com>
2022-03-26 13:02:50 +0000

    Add files via upload

--- test_3d.py
+++ test_3d.py
@@ -10,18 +10,32 @@ from load_data import load
 # df_numpy = np.array([[0., 0., 0.], [1., 1., 1.]])
 # df = pd.DataFrame(df_numpy, columns = ['x','y',"z"])
 
-def plot_data(data):
+def plot_data(data1, data2, data3):
 	fig = go.Figure(data=[go.Scatter3d(
-		x=data[:, 0],
-		y=data[:, 1],
-		z=data[:, 2],
-		mode='markers',
-		marker=dict(
-			size=3,
+		x=data1[:, 0],
+		y=data1[:, 1],
+		z=data1[:, 2],
+		mode='lines',
+		line = dict(
+			#size=size,
 			colorscale='Viridis',
-			opacity=1
 		)
-	)])
+	),go.Scatter3d(
+		x=data2[:, 0],
+		y=data2[:, 1],
+		z=data2[:, 2],
+		mode='lines',
+		line = dict(
+			#size=size,
+			colorscale='Viridis'))
+	,go.Scatter3d(
+		x=data3[:, 0],
+		y=data3[:, 1],
+		z=data3[:, 2],
+		mode='markers',
+		marker = dict(
+			size=20,
+			colorscale='Viridis'))])
 	# fig = px.scatter_3d(df, x='x', y='y', z='z', opacity = 0.5, size_max = 5)
 
 	app = Dash(__name__)
@@ -41,6 +55,11 @@ def plot_data(data):
 
 	app.run_server(debug=True)
 
+def merge_data(data1, data2):
+	return np.append(data1, data2, axis = 0)
+
 if __name__ == '__main__':
-	plot_data(load("envisat"))
+	data = merge_data(load("envisat"), load("globalstar"))
+	data = merge_data(data, np.array([[0, 0, 0]]))
+	plot_data(load("envisat"), load("globalstar"), np.array([[0., 0., 0.]]))
 	
