fd13fa4a36c5c0529eaa0b5b0895e87cc3cef59f
Shrimat <shrimatkapoor@gmail.com>
2022-03-26 17:14:21 +0000

    Adds earth image to the surface

--- load_data.py
+++ load_data.py
@@ -1,6 +1,5 @@
 import numpy as np
 import json
-import pandas as pd
 
 
 def load(satellite):
--- test_3d.py
+++ test_3d.py
@@ -1,49 +1,64 @@
 import plotly.graph_objects as go
 import numpy as np
+from PIL import Image
 
 EARTH_RADIUS = 6_371_000
 
+COLOR_SCALE = [[0.0, 'rgb(30, 59, 117)'],
+              [0.1, 'rgb(46, 68, 21)'],
+              [0.2, 'rgb(74, 96, 28)'],
+              [0.3, 'rgb(115,141,90)'],
+              [0.4, 'rgb(122, 126, 75)'],
+              [0.6, 'rgb(122, 126, 75)'],
+              [0.7, 'rgb(141,115,96)'],
+              [0.8, 'rgb(223, 197, 170)'],
+              [0.9, 'rgb(237,214,183)'],
+              [1.0, 'rgb(255, 255, 255)']]
+
 
 def plot_data(data1, data2):
-	trace = spheres(EARTH_RADIUS, "Earth")
-	fig = go.Figure(data=[go.Scatter3d(
-		x=data1[:, 0],
-		y=data1[:, 1],
-		z=data1[:, 2],
-		mode='lines',
-		name = "Envisat",
-		line = dict(
-			#size=size,
-			color = "orange",
-			colorscale='Viridis',
-		)
-	),go.Scatter3d(
-		x=data2[:, 0],
-		y=data2[:, 1],
-		z=data2[:, 2],
-		mode='lines',
-		name = "Globalstar",
-		line = dict(
-			#size=size,
-			color = "green",
-			colorscale='Viridis'))
-	,trace])
+    texture = np.asarray(Image.open('earth.jpg')).T
+    trace = spheres(EARTH_RADIUS, texture)
+    fig = go.Figure(data=[go.Scatter3d(
+        x=data1[:, 0],
+        y=data1[:, 1],
+        z=data1[:, 2],
+        mode='lines',
+        name="Envisat",
+        line=dict(
+            # size=size,
+            color="orange",
+            colorscale='Viridis',
+        )
+    ), go.Scatter3d(
+        x=data2[:, 0],
+        y=data2[:, 1],
+        z=data2[:, 2],
+        mode='lines',
+        name="Globalstar",
+        line=dict(
+            # size=size,
+            color="green",
+            colorscale='Viridis'))
+        , trace])
 
-	return fig
+    return fig
 
 
-def spheres(size, clr, dist=0):
-	# Set up 100 points. First, do angles
-	theta = np.linspace(0, 2 * np.pi, 100)
-	phi = np.linspace(0, np.pi, 100)
+def spheres(size, texture):
+    N_lat = int(texture.shape[0])
+    N_lon = int(texture.shape[1])
+    # Set up 100 points. First, do angles
+    theta = np.linspace(0, 2 * np.pi, N_lat)
+    phi = np.linspace(0, np.pi, N_lon)
 
-	# Set up coordinates for points on the sphere
-	x0 = dist + size * np.outer(np.cos(theta), np.sin(phi))
-	y0 = size * np.outer(np.sin(theta), np.sin(phi))
-	z0 = size * np.outer(np.ones(100), np.cos(phi))
+    # Set up coordinates for points on the sphere
+    x0 = size * np.outer(np.cos(theta), np.sin(phi))
+    y0 = size * np.outer(np.sin(theta), np.sin(phi))
+    z0 = size * np.outer(np.ones(N_lat), np.cos(phi))
 
-	# Set up trace
-	trace = go.Surface(x=x0, y=y0, z=z0, colorscale=clr)
-	trace.update(showscale=False)
+    # Set up trace
+    trace = go.Surface(x=x0, y=y0, z=z0, surfacecolor=texture, colorscale=COLOR_SCALE)
+    trace.update(showscale=False)
 
-	return trace
+    return trace
